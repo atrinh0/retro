eca02d9277a9abc50a8b943c82074c15058bc67d
An Trinh <atrinh@gmail.com>
2021-06-13 18:44:32 +0100

    Add initial project

---
+++ retro/retro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  retro
+//
+//  Created by An Trinh on 13/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ retro/retro/retroApp.swift
@@ -0,0 +1,17 @@
+//
+//  retroApp.swift
+//  retro
+//
+//  Created by An Trinh on 13/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct retroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
